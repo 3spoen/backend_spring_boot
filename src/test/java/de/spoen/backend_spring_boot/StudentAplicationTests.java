3dd38c5c37667298c80ad6e10685e2bb7b532f36
3spoen <3spoen@gmail.com>
2024-04-06 18:23:32 +0200

    adding the intities and controllers

---
+++ src/test/java/de/spoen/backend_spring_boot/StudentAplicationTests.java
@@ -0,0 +1,13 @@
+package de.spoen.project4;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
