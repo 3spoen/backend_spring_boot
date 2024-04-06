a7d4e152cbf9c686d110d58a577e55f06ed836fe
3spoen <3spoen@gmail.com>
2024-04-06 18:15:46 +0200

    adding the intities and controllers

--- src/main/java/de/spoen/backend_spring_boot/StudentAplication.java
+++ src/main/java/de/spoen/backend_spring_boot/StudentAplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project4Application {
+public class StudentAplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project4Application.class, args);
+		SpringApplication.run(StudentAplication.class, args);
 	}
 
 }
