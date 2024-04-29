6c21060a8fd9e2c1eed695216df62ce01141c310
3spoen <3spoen@gmail.com>
2024-04-29 16:41:29 +0200

    adding : 1.Authanticetion layer for security 2.Error html bages 3.changing the database

--- src/main/java/de/spoen/backend_spring_boot/controller/ChatController.java
+++ src/main/java/de/spoen/backend_spring_boot/controller/ChatController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/chat")
 public class ChatController {
     @Autowired
     private ChatService studentService;
